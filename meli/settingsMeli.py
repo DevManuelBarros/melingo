81a6e6ae84004b06f0d00bcfdd541d642b2301d6
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-10-29 23:52:48 -0300

    prb

--- meli/settingsMeli.py
+++ meli/settingsMeli.py
@@ -5,7 +5,7 @@ Datos de entrada de la aplicación.
 class settingsMeli():
     def __init__(self):
         self.client_id = 12345 #APP ID
-        #Secret Ke
+        #Secret Key
         self.client_secret = 'secret key'
         self.redirect_uri = 'redirect edit when create app'
         self.callbacks = 'callbacks edit when create app'
